1cee132bea8c715f41a3ce8a056ea5fa518110c1
Wangwei <994011918@qq.com>
2021-06-26 22:45:43 +0800

    init

--- app/controller/home.js
+++ app/controller/home.js
@@ -5,16 +5,7 @@ const Controller = require('egg').Controller;
 class HomeController extends Controller {
   async index() {
     const { ctx, service } = this;
-    // const res = await service.home.find(1);
-    // const res = await service.home.create({
-    //   PersonId: '2',
-    //   FirstName: 'qing',
-    //   LastName: 'guiliu'
-    // })
-    // const res = await service.home.update();
-    // ctx.body = res;
-    const res = await service.home.delete();
-    ctx.body = res;
+    ctx.body = 'Hello World';
   }
 }
 
